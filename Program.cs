ea7abdc391a001df65cba9b018bf93d00638d2f1
zlojKolhoznik <73634547+zlojKolhoznik@users.noreply.github.com>
2022-10-02 18:08:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.Run(async context =>
-{
-    Console.WriteLine(context.Request.Method);
-    if (context.Request.Path == "/hello")
-    {
-        await context.Response.WriteAsync("Hello there");
-    }
-    else
-    {
-        context.Response.ContentType = "text/html";
-        await context.Response.SendFileAsync("index.html");
-    }
-});
-await app.RunAsync();
+//app.MapGet("/", () => "Hello World!");
+//app.Run(async context =>
+//{
+ //   Console.WriteLine(context.Request.Method);
+  //  if (context.Request.Path == "/hello")
+ //   {
+  //      await context.Response.WriteAsync("Hello there");
+    //}
+    //else
+    //{
+      //  context.Response.ContentType = "text/html";
+        //await context.Response.SendFileAsync("index.html");
+    //}
+//});
+//await app.RunAsync();
